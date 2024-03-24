2ddc23f75beb234d526eae59b3a60b1721e9fc32
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-03-25 01:20:07 +0900

    バグ修正

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,7 @@
-import Home from "./pages/Home";
+// import Home from "./pages/Home";
 
 function App() {
-  return (
-    <>
-      <Home />
-    </>
-  );
+  return <>{/* <Home /> */}Hello World</>;
 }
 
 export default App;
