c50a1a9417135748ae520c08a123f3765155f367
inoue_r <chru21072@g.nihon-u.ac.jp>
2024-03-25 00:38:53 +0900

    first commit

---
+++ src/pages/Home.tsx
@@ -0,0 +1,32 @@
+import "./home.scss";
+import { useEffect, useState } from "react";
+
+type Schedule = {
+  title?: string;
+  content?: string;
+  time?: string;
+  // place?: string;
+};
+
+const Home = () => {
+  const [schedules, setSchedules] = useState<Schedule>({});
+  useEffect(() => {
+    setSchedules({
+      title: "テスト",
+      content: "応用情報技術者試験",
+      time: "12:00",
+    });
+  }, []);
+
+  return (
+    <div className="box">
+      <div className="box__header">
+        <div className="box__time">{schedules.time}</div>
+        <div className="box__title">{schedules.title}</div>
+      </div>
+      <div className="box__content">{schedules.content}</div>
+    </div>
+  );
+};
+
+export default Home;
